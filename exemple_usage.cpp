7a7a708607da0add52d06d7ae2a557000a9ec5fa
N0zye <84083205+N0zye@users.noreply.github.com>
2024-03-01 19:51:44 +0100

    Update exemple_usage.cpp

--- exemple_usage.cpp
+++ exemple_usage.cpp
@@ -1,14 +1,11 @@
 #include "parser.hpp"
 #include <iostream>
-#include <unordered_map>
-#include <functional>
-using namespace std;
 
 int main() {
 		
 	MathExpression f("3*x+cos(x+2)");
 	f.setExpression("x+cos(x+2)");
-	cout << f(88);
+	std::cout << f(88);
 	
 	return 0;
 }
